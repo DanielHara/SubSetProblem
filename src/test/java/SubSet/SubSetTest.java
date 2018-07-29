265a3d93126b9406d07fff5829f67d0173fd7ff4
Daniel Hara <haradaniel734@gmail.com>
2018-07-28 23:05:34 -0300

    adding quite some more tests

--- src/test/java/SubSet/SubSetTest.java
+++ src/test/java/SubSet/SubSetTest.java
@@ -56,9 +56,9 @@ public class SubSetTest {
         float desiredAverage = 9.5f;
 
         ProblemSet problemSet = new ProblemSet(AlgorithmMode.MELHORMEDIA,
-                numberToChoose,
-                desiredAverage,
-                rates);
+                                               numberToChoose,
+                                               desiredAverage,
+                                               rates);
 
         SubSet subSet = new SubSet();
 
@@ -66,5 +66,65 @@ public class SubSetTest {
         Arrays.sort(solution);
 
         Assert.assertArrayEquals(new float[]{9, 10}, solution, 0.1f);
+    }
+
+    final float ERROR_THRESHOLD = 0.01f;
+
+    public float averageArray(float[] v) {
+        if (v.length == 0)
+            return -1;
+
+        float sum = 0;
+        for (float el: v) {
+            sum = sum + el;
+        }
+        return sum / v.length;
+    }
+
+    @Test
+    public void testBestAverageRealCase() {
+        int numberToChoose = 20;
+        float[] rates = new float[600];
+        float desiredAverage = 9.33f;
+
+        for(int i = 0; i < 100; i++) {
+            rates[i] = 9.23f;
+        }
+
+        for(int i = 100; i < 300; i++) {
+            rates[i] = 9.32f;
+        }
+
+        for(int i = 300; i < 600; i++) {
+            rates[i] = 9.49f;
+        }
+
+        ProblemSet problemSetBestAverage = new ProblemSet(AlgorithmMode.MELHORMEDIA,
+                                                          numberToChoose,
+                                                          desiredAverage,
+                                                          rates);
+
+        ProblemSet problemSetBelowAverage = new ProblemSet(AlgorithmMode.MEDIAPARABAIXO,
+                numberToChoose,
+                desiredAverage,
+                rates);
+
+        ProblemSet problemSetAboveAverage = new ProblemSet(AlgorithmMode.MEDIAPARACIMA,
+                numberToChoose,
+                desiredAverage,
+                rates);
+
+
+        SubSet subSet = new SubSet();
+        float[] solutionBestAverage = subSet.RunAlgorithm(problemSetBestAverage);
+        float[] solutionBelowAverage = subSet.RunAlgorithm(problemSetBelowAverage);
+        float[] solutionAboveAverage = subSet.RunAlgorithm(problemSetAboveAverage);
+
+        Assert.assertEquals(desiredAverage, averageArray(solutionBestAverage), ERROR_THRESHOLD);
+        Assert.assertEquals(desiredAverage, averageArray(solutionBelowAverage), ERROR_THRESHOLD);
+        Assert.assertEquals(desiredAverage, averageArray(solutionAboveAverage), ERROR_THRESHOLD);
+
+        Assert.assertTrue(averageArray(solutionBelowAverage) <= desiredAverage);
+        Assert.assertTrue(averageArray(solutionAboveAverage) >= desiredAverage);
     }
 }
